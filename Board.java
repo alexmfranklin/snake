45539177c468d8ff23a695ce07eb744293d8137e
alexmfranklin <amfa2017@mymail.pomona.edu>
2019-12-01 19:39:31 -0800

    added most functions to the snake

--- Board.java
+++ Board.java
@@ -5,18 +5,50 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private players = new ArrayList<Snake>;
+    private static final int EMPTY = 0;
+    private static final int FOOD = 1;
+    private static final int SNAKE = 2;
 
-    public Board() {
+    private int size;
 
+    private ArrayList<Snake> players = new ArrayList<Snake>;
+    private int[][] board;
+
+    public Board(int size) {
+        this.size = size;
+
+        //initialize the board
+        board = new int[size][size];
+        for(int i = 0; i < size; i ++) {
+            for(int j = 0; j < size; j ++) {
+                board[i][j] = EMPTY;
+            }
+        }
     }
 
-    public void addSnake(Snake snake) {
+    public void addSnake(int x, int y) {
+        Snake snake = new Snake(this, x, y);
+        players.add(snake);
+        board[x][y] = SNAKE;
+    }
 
+    public void updateBoard(int state, int x, int y) {
+        board[x][y] = state;
+    }
+    
+    public int getBoardState(int x, int y) {
+        if(x < size && y < size && x > -1 && size y > -1)
+            return board[x][y];
+        else
+            return -1;
     }
 
     public void takeTurn() {
-        
+
+    }
+
+    public int size() {
+        return this.size;
     }
     
 
