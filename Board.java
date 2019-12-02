5ad133dd80b1e01eb7d6b60cc8a0caf513ea6d12
alexmfranklin <amfa2017@mymail.pomona.edu>
2019-12-01 18:38:18 -0800

    adding board

---
+++ Board.java
@@ -0,0 +1,23 @@
+package snake;
+
+import snake;
+import java.util.ArrayList;
+
+public class Board {
+
+    private players = new ArrayList<Snake>;
+
+    public Board() {
+
+    }
+
+    public void addSnake(Snake snake) {
+
+    }
+
+    public void takeTurn() {
+        
+    }
+    
+
+}
